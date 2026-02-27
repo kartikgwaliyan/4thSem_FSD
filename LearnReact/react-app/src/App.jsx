a4dc9faa1a7e4a23e82b5d7ade827660ab64d74d
Kartik Gwaliyan <kartikgwaliyan123@gmail.com>
2026-02-27 22:09:38 +0530

    updated

--- LearnReact/react-app/src/App.jsx
+++ LearnReact/react-app/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
       <h1>Student ID Card</h1>
       <StudentCard name="Kartik Gwaliyan" branch="CSE" year="2nd"/>
       <StudentCard name="Hemant" branch="CSE" year="3rd"/>
-
     </div>
   );
 }
